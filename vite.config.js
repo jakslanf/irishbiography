ff6d25af06b2d0abe0734a21f37b81b47a783c7a
Finn Jaksland <72381232+jakslanf@users.noreply.github.com>
2023-03-26 22:26:49 +0100

    Modify base for Github pages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://jakslanf.github.io/irishbiography/',
   plugins: [vue()],
   resolve: {
     alias: {
